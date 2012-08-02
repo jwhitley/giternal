e70d688f3b4331e9b80b4e7eccbb3a5510b28c6f
John Whitley <john@luminous-studios.com>
2012-08-01 17:50:10 -0700

    Validate giternal subcommands
    Subcommands are now defined in Giternal::App::Actions, and the app will
    no longer invoke any random App method passed on the command line.  Also
    added some basic reportable error management to decouple App from
    error reporting needs.

--- lib/giternal.rb
+++ lib/giternal.rb
@@ -1,6 +1,7 @@
 module Giternal
 end
 
+require 'giternal/error'
 require 'giternal/repository'
 require 'giternal/yaml_config'
 require 'giternal/app'
--- lib/giternal/app.rb
+++ lib/giternal/app.rb
@@ -2,24 +2,25 @@ require 'giternal'
 
 module Giternal
   class App
+    
+    def self.actions
+      App::Actions.instance_methods(false)
+    end
+
+    def self.action_names
+      actions.map { |m| m.to_s }
+    end
+
     def initialize(base_dir)
       @base_dir = base_dir
     end
 
-    def update(*dirs)
-      if dirs.empty?
-        config.each_repo {|r| r.update }
-      else
-        dirs.each do |dir|
-          if repo = config.find_repo(dir)
-            repo.update
-          end
-        end
-      end
-    end
-
     def run(action, *args)
-      send(action, *args)
+      if self.class.action_names.include?(action)
+        send(action, *args)
+      else
+        raise Giternal::Error::UnknownCommand, action
+      end
     end
 
     def config
@@ -36,5 +37,22 @@ module Giternal
 
       @config = YamlConfig.new(@base_dir, File.read(config_file))
     end
+
+    module Actions
+      def update(*dirs)
+        if dirs.empty?
+          config.each_repo {|r| r.update }
+        else
+          dirs.each do |dir|
+            if repo = config.find_repo(dir)
+              repo.update
+            end
+          end
+        end
+      end
+    end
+
+    include Actions
+
   end
 end
